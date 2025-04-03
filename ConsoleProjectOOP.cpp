f690ad376300fabef9aa1d481622b95c210ab869
ShatalovSI <shatalov_s_i@mail.ru>
2025-04-03 10:07:13 +0300

    update

--- ConsoleProjectOOP.cpp
+++ ConsoleProjectOOP.cpp
@@ -1,8 +1,14 @@
 ﻿#include <iostream>
 #include "Console.h"
 
-
-
 int main()
 {
+	Console console{ "Ny console" };
+	console.SetForeColor(Color::Green, 1);
+	console.SetBackColor(Color::Gray, 0);
+
+	console.Rectangle(1, 1, 5, 10, 'q');
+
+	console.Rect(1, 1, 5, 10, 'Я');
+	std::cin.get();
 }
